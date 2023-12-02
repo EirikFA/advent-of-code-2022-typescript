3a79553b0d70120ec3853a18c88ba49940176f16
Eirik <eiriand03@gmail.com>
2023-12-02 14:49:26 +0100

    Improve CLI

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import Solver from "./solvers/Solver.js";
 
+let day: number;
+
 if (process.argv.length === 2) {
-  console.error("A day number is required");
-  process.exit(1);
+  const now = new Date();
+  if (now.getMonth() !== 11) {
+    console.error("It's not December");
+    process.exit(1);
+  }
+
+  day = new Date().getDate();
+  if (day > 25) {
+    console.error("It's past Christmas");
+    process.exit(1);
+  }
+} else {
+  day = Number(process.argv[2]);
+  if (Number.isNaN(day)) {
+    console.error("Day must be a number");
+    process.exit(1);
+  }
 }
 
-const day = Number(process.argv[2]);
-if (Number.isNaN(day)) {
-  console.error("Day must be a number");
-  process.exit(1);
-}
-
+console.log("--- Day", day, "---");
 const solver = await Solver.create(day);
 await solver.run();
